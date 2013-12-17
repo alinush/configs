f2fbc241cf73c76c95059d4e037d344001c8fed8
Alin Tomescu <alinush@mit.edu>
2013-12-17 11:15:43 -0500

    added colordiff to the diff.sh script

--- diff.sh
+++ diff.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 scriptdir=$(readlink -f $(dirname $0))
 confdir=$scriptdir/conf-files
+diff=`which colordiff`
+[ -z "$diff" ] && diff=`which diff`
 
 echo
 echo "Configuration files in conf-files/"
@@ -11,6 +14,6 @@ echo "Diffs (yours vs. repo)"
 echo "======================"
 for f in `ls -A $confdir/`; do
     echo " * Diff for ~/$f and conf-files/$f"
-    diff -rupN ~/$f $confdir/$f
+    $diff -rupN ~/$f $confdir/$f
     echo
 done
