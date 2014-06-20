e6633a016416ab429c6049205cbfe30cc54ba23d
Alin Tomescu <alinush@mit.edu>
2014-06-19 22:14:24 -0400

    added ubuntu detection code

--- install-vim-markdown.sh
+++ install-vim-markdown.sh
@@ -1,8 +1,14 @@
 set -e
 
 cd /tmp
-sudo apt-get install -y vim-addon-manager
-git clone https://github.com/plasticboy/vim-markdown.git
-cd vim-markdown
-sudo make install
-vim-addon-manager install mkd
+
+if cat /etc/issue | grep Ubuntu >/dev/null; then
+    echo
+    echo "Detected Ubuntu using /etc/issue"
+    echo
+    sudo apt-get install -y vim-addon-manager
+    git clone https://github.com/plasticboy/vim-markdown.git
+    cd vim-markdown
+    sudo make install
+    vim-addon-manager install mkd
+fi
