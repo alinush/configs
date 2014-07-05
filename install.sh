1ae8591ff9a6a9e432916f3c21d1447a011c0f4f
Alin Tomescu <alinush@mit.edu>
2014-07-05 10:53:46 -0400

    created ~/.backup/vim/swap/ directory for vim

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ scriptdir=$(readlink -f $(dirname $0))
 [ -f ~/.bash_aliases ] && mv ~/.bash_aliases ~/.bash_aliases_old
 ln -s $scriptdir/conf-files/bash/.bash_aliases ~/.bash_aliases
 
+mkdir -p ~/.backup/vim/swap
 [ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc_old
 ln -s $scriptdir/conf-files/vim/.vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
