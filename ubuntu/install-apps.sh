22a0161aca97761ffac245fb4bbfff60f2f1d226
Alin Tomescu <alinush@mit.edu>
2014-07-12 16:23:18 -0400

    added ubuntu scripts

---
+++ ubuntu/install-apps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Remove a few of these: Rhythmbox
+#echo "Removing some crappy apps..."
+#sudo apt-get remove rhythmbox
+
+# VLC, git, svn
+echo "Installing some new apps..."
+sudo apt-get install vim minicom subversion git libxss1 gedit-plugins build-essential vlc apt-file indicator-cpufreq eclipse eclipse-cdt gpointing-device-settings
