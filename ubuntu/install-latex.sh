2051ee727e961cf156274be5362036440026ad11
Alin Tomescu <alinush@mit.edu>
2014-08-24 01:59:14 -0400

    latex changes

--- ubuntu/install-latex.sh
+++ ubuntu/install-latex.sh
@@ -1,3 +1,3 @@
 alias agi='sudo apt-get install'
 
-agi pandoc texlive tex-common tex-gyre texinfo texlive texlive-base texlive-bibtex-extra texlive-binaries texlive-extra-utils texlive-font-utils texlive-fonts-recommended texlive-fonts-recommended-doc texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pstricks texlive-pstricks-doc latex-beamer latex-xcolor
+agi pandoc texlive tex-common tex-gyre texinfo texlive texlive-base texlive-bibtex-extra texlive-binaries texlive-extra-utils texlive-font-utils texlive-fonts-recommended texlive-fonts-recommended-doc texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pstricks texlive-pstricks-doc latex-beamer latex-xcolor texlive-metapost texlive-latex-extra texlive-doc-en
