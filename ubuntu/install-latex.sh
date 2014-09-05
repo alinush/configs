c28bb8852017f1e7429a4991ecf60e0ac0014933
Alin Tomescu <alinush@mit.edu>
2014-09-05 14:44:52 -0400

    got rid of agi alias

--- ubuntu/install-latex.sh
+++ ubuntu/install-latex.sh
@@ -1,3 +1,1 @@
-alias agi='sudo apt-get install'
-
-agi pandoc texlive tex-common tex-gyre texinfo texlive texlive-base texlive-bibtex-extra texlive-binaries texlive-extra-utils texlive-font-utils texlive-fonts-recommended texlive-fonts-recommended-doc texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pstricks texlive-pstricks-doc latex-beamer latex-xcolor texlive-metapost texlive-latex-extra texlive-doc-en
+sudo apt-get install pandoc texlive tex-common tex-gyre texinfo texlive texlive-base texlive-bibtex-extra texlive-binaries texlive-extra-utils texlive-font-utils texlive-fonts-recommended texlive-fonts-recommended-doc texlive-generic-recommended texlive-latex-base texlive-latex-base-doc texlive-latex-recommended texlive-latex-recommended-doc texlive-luatex texlive-pstricks texlive-pstricks-doc latex-beamer latex-xcolor texlive-metapost texlive-latex-extra texlive-doc-en
