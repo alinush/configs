3fc8fe204fe859169ecb6dbb97f18d4255107452
Alin Tomescu <alinush@mit.edu>
2014-10-10 22:10:34 -0400

    g++ now works with grc

--- conf-files/bash/setup-grc-aliases.sh
+++ conf-files/bash/setup-grc-aliases.sh
@@ -1,13 +1,13 @@
 #######
 # GRC #
 #######
-alias gcc='grc gcc'
+alias gcc='grc -es gcc'
 alias make='grc make'
 alias configure='grc ./configure'
 alias ping='grc ping'
 alias dig='grc dig'
 alias last='grc last'
-alias g++='grc g++'
+alias g++='grc -es g++'
 alias traceroute='grc traceroute'
 alias ld='grc ld'
 alias df='grc df'
