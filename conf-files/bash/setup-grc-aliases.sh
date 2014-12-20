2d184fe751fe0c4a57869024ad056d0337795fc5
Alin Tomescu <alinush@mit.edu>
2014-12-20 02:10:28 -0500

    fixed grc aliases when grc is not installed

--- conf-files/bash/setup-grc-aliases.sh
+++ conf-files/bash/setup-grc-aliases.sh
@@ -1,13 +1,18 @@
 #######
 # GRC #
 #######
-alias gcc='grc -es gcc'
-alias make='grc make'
-alias configure='grc ./configure'
-alias ping='grc ping'
-alias dig='grc dig'
-alias last='grc last'
-alias g++='grc -es g++'
-alias traceroute='grc traceroute'
-alias ld='grc ld'
-alias df='grc df'
+
+# Check if grc is installed before creating potentially
+# unusable aliases
+if grc &>/dev/null; then
+    alias gcc='grc -es gcc'
+    alias make='grc make'
+    alias configure='grc ./configure'
+    alias ping='grc ping'
+    alias dig='grc dig'
+    alias last='grc last'
+    alias g++='grc -es g++'
+    alias traceroute='grc traceroute'
+    alias ld='grc ld'
+    alias df='grc df'
+fi
