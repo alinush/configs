e0b58e060589ff45c030ace51e41834081c889fb
Alin Tomescu <alinush@mit.edu>
2014-12-20 01:11:00 -0500

    added git fix

--- install-git.sh
+++ install-git.sh
@@ -2,6 +2,12 @@
 
 set -e
 scriptdir=$(readlink -f $(dirname $0))
+conffile=.gitconfig
+
+if git --version | grep 1\.7; then
+    conffile=.gitconfig-1.7
+    echo "Detected older version of git, using .gitconfig file: $conffile" 
+fi
 
 [ -f ~/.gitconfig ] && mv ~/.gitconfig ~/.gitconfig.`date +%s`
-ln -s $scriptdir/conf-files/.gitconfig ~/.gitconfig
+ln -s $scriptdir/conf-files/$conffile ~/.gitconfig
