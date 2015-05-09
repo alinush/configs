4f5c8232390272631fc90a50dbafdcd6a96b4f83
Alin Tomescu <alinush@mit.edu>
2015-05-09 18:48:45 -0400

    added recoll to app lis

--- ubuntu/install-apps.sh
+++ ubuntu/install-apps.sh
@@ -9,4 +9,5 @@ echo "Installing some new apps..."
 sudo apt-get install -y vim minicom subversion git libxss1 gedit-plugins \
     build-essential vlc apt-file indicator-cpufreq eclipse eclipse-cdt \
     gpointing-device-settings htop iotop xclip compizconfig-settings-manager \
-    compiz-plugins compiz-plugins-extra markdown colordiff dos2unix
+    compiz-plugins compiz-plugins-extra markdown colordiff dos2unix recoll \
+
