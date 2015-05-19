ba4d45481eeee01dbf844b582294fe9259331ca2
Alin Tomescu <alinush@mit.edu>
2015-05-19 06:09:10 -0400

    yes

--- install-recoll.sh
+++ install-recoll.sh
@@ -4,7 +4,7 @@ set -e
 scriptdir=$(readlink -f $(dirname $0))
 conffile=recoll.conf
 
-sudo apt-get install recoll python-recoll pstotext
+sudo apt-get -y install recoll python-recoll pstotext
 
 [ -d ~/.recoll ] || { echo "ERROR: Recoll directory not present in ~/.recoll"; exit 1; }
 
