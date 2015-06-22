e1c283e506c8f551a4cba3709634aae2a1c15a02
Alin Tomescu <alinush@mit.edu>
2015-06-22 10:36:27 -0400

    different install-apps.sh for CLI and GUI ubuntu installations

---
+++ ubuntu/install-cli-apps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Installing some new apps..."
+sudo apt-get install -y vim \
+    minicom \
+    subversion \
+    git \
+    libxss1 \
+    apt-file \
+    htop \
+    iotop \
+    xclip \
+    markdown \
+    colordiff \
+    dos2unix \
+    jhead \
+    exiftool \
+    youtube-dl \
--- ubuntu/install-gnome-apps.sh
+++ ubuntu/install-gnome-apps.sh
@@ -4,28 +4,13 @@
 #echo "Removing some crappy apps..."
 #sudo apt-get remove rhythmbox
 
-# VLC, git, svn
 echo "Installing some new apps..."
-sudo apt-get install -y vim \
-    minicom \
-    subversion \
-    git \
-    libxss1 \
+sudo apt-get install -y \
     gedit-plugins \
     vlc \
-    apt-file \
     indicator-cpufreq \
     gpointing-device-settings \
-    htop \
-    iotop \
-    xclip \
     compizconfig-settings-manager \
     compiz-plugins \
     compiz-plugins-extra \
-    markdown \
-    colordiff \
-    dos2unix \
     recoll \
-    jhead \
-    exiftool \
-    youtube-dl \
