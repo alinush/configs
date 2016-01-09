ae9a529c444fc9f1c22917304f42a2af049f79a8
Alin Tomescu <alinush@mit.edu>
2016-01-09 11:24:32 -0500

    added htop

--- osx/install-brew.sh
+++ osx/install-brew.sh
@@ -1,3 +1,9 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install bash-completion coreutils git grc colordiff wget
+brew install bash-completion \
+    coreutils \
+    git \
+    grc \
+    colordiff \
+    wget \
+    htop
