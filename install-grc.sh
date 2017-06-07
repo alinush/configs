5c49e5681c9bb046b328a1cca642df2a7b39fb33
Alin Tomescu <alinush@mit.edu>
2017-06-06 17:24:43 -0700

    quotes around path in install-grc.sh

--- install-grc.sh
+++ install-grc.sh
@@ -3,4 +3,4 @@ scriptdir=$( cd $(dirname $0) ; pwd -P )
 
 [ "$OS" = "Linux" -a "$OS_FLAVOR" = "Ubuntu" ] && sudo apt-get install grc
 
-ln -s $scriptdir/conf-files/grc ~/.grc
+ln -s "$scriptdir/conf-files/grc" ~/.grc
