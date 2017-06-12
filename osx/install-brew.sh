d41fe1d771f5fbd6230862f7e5728ae73a0dde95
Alin Tomescu <alinush@mit.edu>
2017-06-11 21:27:47 -0700

    added OS X utils installation script separately from brew installation script

--- osx/install-brew.sh
+++ osx/install-brew.sh
@@ -1,13 +1,1 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-brew install bash-completion \
-    coreutils \
-    git \
-    grc \
-    colordiff \
-    wget \
-    htop \
-    youtube-dl \
-    ffmpeg \
-    vim \
-    gnu-sed \
---
+++ osx/install-utils.sh
@@ -0,0 +1,12 @@
+brew install bash-completion \
+    coreutils \
+    git \
+    grc \
+    colordiff \
+    wget \
+    htop \
+    youtube-dl \
+    ffmpeg \
+    vim \
+    gnu-sed \
+    gawk \
