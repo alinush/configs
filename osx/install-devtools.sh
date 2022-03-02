101aaf3298b852fb4510acf54c702e5b4196d988
Alin Tomescu <alinush@mit.edu>
2022-03-02 11:39:34 -0800

    no more pip

--- osx/install-devtools.sh
+++ osx/install-devtools.sh
@@ -1,13 +1,6 @@
 set -e
 cd /tmp
 
-## Install Python's PIP
-if ! which pip; then
-    wget https://bootstrap.pypa.io/get-pip.py
-    chmod +x get-pip.py
-    sudo ./get-pip.py
-fi
-
 brew install gdb \
     six \
     gmp \
